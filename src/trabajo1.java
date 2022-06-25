f7aecf5466cc69a933981b9298dcd422fa0df412
jaimito1995 <jaimecaicedo.jc@hotmail.com>
2022-06-25 09:18:59 -0500

    trabajo 1

---
+++ src/trabajo1.java
@@ -0,0 +1,14 @@
+public class trabajo1 {
+    public static void main(String args[]){
+        for (int i = 1; i <= 6; i++) {
+            int E;
+            if(i%2==0) {
+                E = 3;
+            }else {
+                E = 1;
+            }
+            System.out.println("numero:"+E);
+            }
+        }
+    }
+
